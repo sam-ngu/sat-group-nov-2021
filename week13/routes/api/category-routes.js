111944b2269097d19a27951e00c0a4358f0b3435
Sam <opensource@acadea.io>
2022-03-12 09:44:16 +0800

    added update endpoint for category

--- week13/routes/api/category-routes.js
+++ week13/routes/api/category-routes.js
@@ -39,6 +39,17 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
+  Category.update({
+    category_name: req.body.category_name
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then((updated) => {
+    res.json(updated);
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
